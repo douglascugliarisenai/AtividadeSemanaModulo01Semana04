51eebb0cf1bfbd91b3e0d48a62808185ac1c76c9
douglascugliarisenai <douglas_s5@estudante.sc.senai.br>
2024-02-14 19:39:35 -0300

    Adicionando Exercicio 09 Semana 04 do Trello

---
+++ M1S4Exercicios/Exercicio09/ReadMe.md
@@ -0,0 +1,11 @@
+<h1 align="center"> FuturoDev [Fitness] </h1>
+
+<h2 align="center"> Exercícios da Semana 4</h2>
+
+<h1 align="center">
+    <a href="https://floripamaistec.pmf.sc.gov.br/">🔗 Floripa Mais Tec</a>
+</h1>
+<p align="center">🚀 Exercício 09: Switch/case</p>
+
+<p>Use um switch para exibir mensagens diferentes com base em uma condição.</p>
+
---
+++ M1S4Exercicios/Exercicio09/script.js
@@ -0,0 +1,29 @@
+let diaSemana = Math.round(Math.random() * 6)
+
+console.log('Dia em número selecionado '+diaSemana)
+
+switch(diaSemana) {
+    case 0:
+        console.log("Domingo");
+        break;
+    case 1:
+        console.log("Segunda-feira");
+        break;
+    case 2:
+        console.log("Terça-feira");
+        break;
+    case 3:
+        console.log("Quarta-feira");
+        break;
+    case 4:
+        console.log("Quinta-feira");
+        break;
+    case 5:
+        console.log("Sexta-feira");
+        break;
+    case 6:
+        console.log("Sábado");
+        break;
+    default:
+        console.log("Número inválido");
+}
