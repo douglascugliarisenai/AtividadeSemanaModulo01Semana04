952bf68f13aa79a1688a82cb414f3007b37e0be4
douglascugliarisenai <douglas_s5@estudante.sc.senai.br>
2024-02-14 18:18:54 -0300

    Adicionando Exercicio 06 Semana 04 do Trello

--- M1S4Exercicios/Exercicio05/script.js
+++ M1S4Exercicios/Exercicio05/script.js
@@ -1,6 +1,4 @@
-/* Para resolução deste problema utilizamos a classe Math onde utilizamos os métodos
- Round para aredondamento e Random para gerar um valor numerico
-*/
+
 const min = -10
 const max = 10
 let numeroSelecionado = Math.round(Math.random() * (max - min + 1) + min)
